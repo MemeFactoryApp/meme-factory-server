75371c1c111c3d806f4f9fa83a1a46e23890b6ad
Emily Stevenson <e.stevenson6@hotmail.co.uk>
2023-07-03 13:31:55 +0100

    adding group model

---
+++ models/Group.model.js
@@ -0,0 +1,13 @@
+const { Schema, model } = require("mongoose");
+
+const groupSchema = new Schema({
+    // this is the single embedded document and recommended approach for 1:1 relationships
+    groupName: String,
+    users: [String],
+    memes: [String],
+    createdBy: { type: Schema.Types.ObjectId, ref: 'User' }
+});
+
+const Group = model("Group", groupSchema);
+
+module.exports = Group;
